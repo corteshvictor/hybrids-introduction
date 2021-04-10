976e96b67cdd50dd009096598b4ffdda4ab5e6db
Victor Cortés <cortes.victorhugo@gmail.com>
2021-04-10 03:36:51 -0500

    upgrade dependencies and remove twitter cookies

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: __dirname + "/build",
+    path: `${__dirname}/build`,
     filename: "bundle.js",
   },
   module: {
@@ -11,6 +11,13 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        loader: "url-loader",
+        options: {
+          limit: 8192,
+        },
       },
     ],
   },
